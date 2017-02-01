57a47a45b89afff296812273cf41677bac441446
KyomaHooin <richard_bruna@nm.cz>
2017-02-01 20:21:23 +0100

    CIE 1976 basic.

--- color/col.py
+++ color/col.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+#no display..
 import matplotlib
 matplotlib.use('Agg')
 
@@ -8,17 +9,9 @@ from colour import ILLUMINANTS_RELATIVE_SPDS
 from colour.plotting import *
 from colour.utilities.verbose import message_box
 
-#colour.plotting.DEFAULT_FIGURE_WIDTH = 5
-
-#message_box('Plotting "CIE 1931 Chromaticity Diagram".')
-#CIE_1931_chromaticity_diagram_plot()
-
-#A = ILLUMINANTS_RELATIVE_SPDS['A']
 #D65 = ILLUMINANTS_RELATIVE_SPDS['D65']
 
-#CIE_1931_chromaticity_diagram_plot(filename='test.png',figure_size=(10,5))
+CIE_1931_chromaticity_diagram_plot(filename='1931.png',figure_size=(10,6),title='CIE 1931 Chromaticity Diagram')
 
-#colour.plotting.common.DEFAULT_FONT_SIZE=5
-
-CIE_1931_chromaticity_diagram_plot(filename='test.png',figure_size=(10,6),title='CIE 1931 Chromaticity Diagram')
+CIE_1976_UCS_chromaticity_diagram_plot(filename='1976.png',figure_size=(10,6),title='CIE 1976 Chromaticity Diagram')
 
