99a623a8ea195deda98def349091f10875dc1985
KyomaHooin <richard_bruna@nm.cz>
2017-02-05 20:42:07 +0100

    Title, single color.

--- color/color.py
+++ color/color.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python
+#
+# TODO:
+#
+# CSV loop
+#
 
 import matplotlib
 matplotlib.use('Agg')# no display
 
+import numpy as np
+
 from colour.plotting import *
+from colour.models import (Lab_to_XYZ,XYZ_to_sRGB)
 
 #
 
+tab_name='I 686' 
 Lab_L=47.8
 Lab_a=11.3
 Lab_b=15.4
 
 CIE_1976_UCS_chromaticity_diagram_plot(Lab_L, Lab_a, Lab_b,\
-	filename='1976.png', \
+	filename=tab_name.replace(' ','') + '_1976.png', \
 	figure_size=(10,6), \
-	title='CIE 1976 Chromaticity Diagram - Hneda' \
+	title='CIE 1976 Chromaticity Diagram - ' + tab_name, \
 	)
 
+#RGB = (0.32315746, 0.32983556, 0.33640183)
+
+#Lab = np.array([Lab_L, Lab_a, Lab_b])
+#Lab_sRGB =XYZ_to_sRGB(Lab_to_XYZ(Lab))
+
+#print Lab_sRGB
+
+#single_colour_plot(ColourParameter(Lab_sRGB), \
+#	filename='color.png', \
+#	figure_size=(10,6), \
+#	title='Lab to sRGB color' \
+#	)
