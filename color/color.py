f93a5766a0c99bb6a9cd64c90ebc532e7fa6b484
KyomaHooin <richard_bruna@nm.cz>
2017-02-06 16:23:06 +0100

    Color update.

--- color/color.py
+++ color/color.py
@@ -11,30 +11,31 @@ matplotlib.use('Agg')# no display
 import numpy as np
 
 from colour.plotting import *
-from colour.models import (Lab_to_XYZ,XYZ_to_sRGB)
+from colour.models import Lab_to_XYZ,XYZ_to_sRGB
 
-#
+#----
 
 tab_name='I 686' 
 Lab_L=47.8
 Lab_a=11.3
 Lab_b=15.4
 
-CIE_1976_UCS_chromaticity_diagram_plot(Lab_L, Lab_a, Lab_b,\
+Lab = np.array([Lab_L, Lab_a, Lab_b])
+
+illuminant=DEFAULT_PLOTTING_ILLUMINANT# D65
+
+Lab_sRGB = XYZ_to_sRGB(Lab_to_XYZ(Lab,illuminant),illuminant)
+
+#----
+
+CIE_1976_UCS_chromaticity_diagram_plot(Lab,\
 	filename=tab_name.replace(' ','') + '_1976.png', \
 	figure_size=(10,6), \
 	title='CIE 1976 Chromaticity Diagram - ' + tab_name, \
 	)
 
-#RGB = (0.32315746, 0.32983556, 0.33640183)
-
-#Lab = np.array([Lab_L, Lab_a, Lab_b])
-#Lab_sRGB =XYZ_to_sRGB(Lab_to_XYZ(Lab))
-
-#print Lab_sRGB
-
-#single_colour_plot(ColourParameter(Lab_sRGB), \
-#	filename='color.png', \
-#	figure_size=(10,6), \
-#	title='Lab to sRGB color' \
-#	)
+single_colour_plot(ColourParameter(RGB=Lab_sRGB), \
+	filename=tab_name.replace(' ','') + '_sRGB.png', \
+	figure_size=(4,4), \
+	title='Lab to sRGB color' \
+	)
