0bd474083b5e707f092d574cae71acdbc5c428e5
KyomaHooin <richard_bruna@nm.cz>
2017-07-12 19:34:23 +0200

    Element iterate.

--- diagram/diagram.py
+++ diagram/diagram.py
@@ -2,17 +2,13 @@
 
 import StringIO,ternary,numpy,sys
 
+from itertools import combinations
 from matplotlib import pyplot
-
-try:
-	f = open('input_color.csv','r')
-except:
-	print("Failed to read input.")
-	sys.exit(1)
-
 
 clr = ('#F3C300','#875692','#F38400','#A1CAF1','#BE0032','#C2B280','#848482','#008856','#E68FAC','#0067A5',
 	'#F99379','#604E97','#F6A600','#B3446C','#DCD300','#882D17','#8DB600','#654522','#E25822','#2B3D26')
+
+element = ('Na','Mg','Al','Si','P','K','Ca','Ti','Mn','Fe')
 
 #---------------------------
 
@@ -49,7 +45,36 @@ def parse_data(data,sel):
 
 	return numpy.concatenate((out,std[:,None]),-1)
 
-def scatter_data(data,std):
+def plot_data(src,elm):
+
+	data = parse_data(src,elm)
+
+	std = numpy.unique(data[1:,-1])
+
+	figure, ax = pyplot.subplots(figsize=(8,8), facecolor='white')
+	tax = ternary.TernaryAxesSubplot(ax=ax, scale=100)
+
+	tax.gridlines(color="blue", multiple=5, zorder=-1)
+	tax.boundary(linewidth=1.25)
+
+	tax.ticks(linewidth=2, multiple=10, offset=0.014)
+
+	ax.axis('off')
+
+	ax.annotate("", xy=(7.5,25), xytext=(25,55), arrowprops=dict(arrowstyle="->"))
+	ax.annotate("", xy=(30,-6), xytext=(70,-6), arrowprops=dict(arrowstyle="<-"))
+	ax.annotate("", xy=(92.5,25), xytext=(75,55), arrowprops=dict(arrowstyle="<-"))
+
+	tax.right_corner_label(elm[0] + '[%]', fontsize=20, offset=0.01)
+	tax.left_corner_label(elm[2] + '[%]', fontsize=20, offset=0.01)
+	tax.top_corner_label(elm[1] + '[%]', fontsize=20, offset=0.2)
+
+	tax.left_axis_label(elm[2] + " [%]", fontsize=12, offset=0.12)
+	tax.right_axis_label(elm[1] + " [%]", fontsize=12, offset=0.12)
+	tax.bottom_axis_label(elm[0] + " [%]", fontsize=12, offset=0)
+
+	tax._redraw_labels()
+
 	for st in std:
 		tax.scatter(
 			data[data[:,3] == st,:3].astype(float),
@@ -61,9 +86,21 @@ def scatter_data(data,std):
 			label=st
 		)
 
+	tax.legend(frameon=False, scatterpoints=1, handletextpad=0, bbox_to_anchor = (1.08, 1.15))
+
+	ternary.plt.subplots_adjust(left=0.08, right=0.9, top=0.85, bottom=0.06)
+
+	tax.savefig(filename='_'.join(elm) + '_diagram.png', format='png', dpi=300)
+	
+	ternary.plt.close()
+
 #---------------------------
 
-#VAR
+try:
+	f = open('input_color.csv','r')
+except:
+	print("Failed to read input.")
+	sys.exit(1)
 
 if is_valid_csv(f):
 	raw = parse_csv(f)
@@ -76,60 +113,6 @@ subset = numpy.delete(subset,0,1)
 subset = numpy.delete(subset,0,1)
 subset = numpy.delete(subset,range(1,numpy.shape(subset)[1],2),1)# SD
 
-element=('Mg','Al','Fe')
-
-data = parse_data(subset,element)
-
-std = numpy.unique(data[1:,-1])
-
-#BASE
-
-figure, ax = pyplot.subplots(figsize=(8,8), facecolor='white')
-tax = ternary.TernaryAxesSubplot(ax=ax, scale=100)
-
-#GRID/BOUNDARY
-
-tax.gridlines(color="blue", multiple=5, zorder=-1)
-tax.boundary(linewidth=1.25)
-
-#TICK
-
-tax.ticks(linewidth=2, multiple=10, offset=0.014)
-
-#AXIS
-
-ax.axis('off')
-
-#ARROW
-
-ax.annotate("", xy=(7.5,25), xytext=(25,55), arrowprops=dict(arrowstyle="->"))
-ax.annotate("", xy=(30,-6), xytext=(70,-6), arrowprops=dict(arrowstyle="<-"))
-ax.annotate("", xy=(92.5,25), xytext=(75,55), arrowprops=dict(arrowstyle="<-"))
-
-#LABEL
-
-tax.right_corner_label(element[0] + '[%]', fontsize=20, offset=0.01)
-tax.left_corner_label(element[2] + '[%]', fontsize=20, offset=0.01)
-tax.top_corner_label(element[1] + '[%]', fontsize=20, offset=0.2)
-
-tax.left_axis_label(element[2] + " [%]", fontsize=12, offset=0.12)
-tax.right_axis_label(element[1] + " [%]", fontsize=12, offset=0.12)
-tax.bottom_axis_label(element[0] + " [%]", fontsize=12, offset=0)
-
-tax._redraw_labels()
-
-#SCATTER
-
-scatter_data(data, std)
-
-#LEGEND
-
-tax.legend(frameon=False, scatterpoints=1, handletextpad=0, bbox_to_anchor = (1.08, 1.15))
-
-#SAVE/DISPLAY
-
-ternary.plt.subplots_adjust(left=0.08, right=0.9, top=0.85, bottom=0.06)
-
-tax.show()
-#tax.savefig(filename='demo.png', format='png', dpi=300)
+for i in combinations(element, 3):
+	plot_data(subset,i)
 
